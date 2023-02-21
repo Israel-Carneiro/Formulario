f6cc2ea2d94f9e1ddabeb8ee69abee18de0f2bb5
Israel-Carneiro <raelc.oliveira@gmail.com>
2023-02-21 20:40:10 -0300

    Variáveis para preenchimento automático

--- assets/js/buscaCepViaAPI.js
+++ assets/js/buscaCepViaAPI.js
@@ -2,5 +2,14 @@ async function buscaEndereco (cep) {
     try {
         var consultaCEP = await fetch(`https://viacep.com.br/ws/${cep}/json/`)
         var CEPConvertido = await consultaCEP.json();
+
+        var logradouro = document.getElementById('endereco');
+        var bairro = document.getElementById('bairro');
+        var cidade = document.getElementById('cidade');
+        var estado = document.getElementById('estado');
+        logradouro.value = CEPConvertido.logradouro;
+        bairro.value = CEPConvertido.bairro;
+        cidade.value = CEPConvertido.localidade;
+        estado.value = CEPConvertido.uf;
     }
 }
