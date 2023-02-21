7c0f2f9fe5cff4b0cebf2a3673d2fc112ef85195
Israel-Carneiro <raelc.oliveira@gmail.com>
2023-02-21 20:42:05 -0300

    Mensagem de erro para CEP inexistente

--- assets/js/buscaCepViaAPI.js
+++ assets/js/buscaCepViaAPI.js
@@ -2,6 +2,9 @@ async function buscaEndereco (cep) {
     try {
         var consultaCEP = await fetch(`https://viacep.com.br/ws/${cep}/json/`)
         var CEPConvertido = await consultaCEP.json();
+        if (CEPConvertido.erro) {
+            throw Error('CEP inexistente!');
+        }
 
         var logradouro = document.getElementById('endereco');
         var bairro = document.getElementById('bairro');
