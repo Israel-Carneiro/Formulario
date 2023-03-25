9bd974870b2bc46eab87fa77c6e5e06c2168c9a0
Israel-Carneiro <raelc.oliveira@gmail.com>
2023-03-25 19:43:13 -0300

    Função para validar primeiro dígito validador

--- assets/js/validaCPF.js
+++ assets/js/validaCPF.js
@@ -18,3 +18,21 @@ function validaNumerosRepetidos(cpf) {
 
     return numerosRepetidos.includes(cpf);
 }
+
+function validaPrimeiroDigito(cpf) {
+    let soma = 0;
+    let multiplicador = 10;
+
+    for(let tamanho = 0; tamanho < 9; tamanho++) {
+        soma += cpf[tamanho] * multiplicador;
+        multiplicador--;
+    }
+
+    soma = (soma * 10) % 11;
+
+    if (soma == 10 || soma == 11) {
+        soma = 0;
+    }
+
+    return soma != cpf[9];
+}
