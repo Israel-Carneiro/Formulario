0345aa8b91e4e9a3b3e6e5b906f32ce8e715fab3
Israel-Carneiro <raelc.oliveira@gmail.com>
2023-03-25 19:55:52 -0300

    Chamando função para validar CPF

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,3 +3,9 @@ const camposDoFormulario = document.querySelectorAll("[required]");
 camposDoFormulario.forEach((campo) => {
     campo.addEventListener("blur", () => verificaCampo(campo));
 })
+
+function verificaCampo(campo) {
+    if (campo.name == "cpf" && campo.value.length >= 11) {
+        ehUmCPF(campo);
+    }
+}
