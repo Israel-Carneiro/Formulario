0b284064209a5ea5ca8cebc935462542e8a3538a
Israel-Carneiro <raelc.oliveira@gmail.com>
2023-03-27 20:38:29 -0300

    Função para calcular idade atual.

--- assets/js/validaIdade.js
+++ assets/js/validaIdade.js
@@ -1,3 +1,10 @@
 function ehMaiorDeIdade(campo) {
     const dataNascimento = new Date(campo.value);
 }
+
+function validaIdade(data) {
+    const dataAtual = new Date();
+    const dataMais18 = new Date(data.getUTCFullYear() + 18, data.getUTCMonth(), data.getUTCDate());
+
+    return dataAtual >= dataMais18;
+}
