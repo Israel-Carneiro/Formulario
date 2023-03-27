b7042e4c13397b63d5d254df23c95018ba5a0ef7
Israel-Carneiro <raelc.oliveira@gmail.com>
2023-03-27 20:39:28 -0300

    Mensagem para menores de idade.

--- assets/js/validaIdade.js
+++ assets/js/validaIdade.js
@@ -1,5 +1,8 @@
 function ehMaiorDeIdade(campo) {
     const dataNascimento = new Date(campo.value);
+    if (!validaIdade(dataNascimento)) {
+        campo.setCustomValidity('O usuário não é maior de idade');
+    }
 }
 
 function validaIdade(data) {
