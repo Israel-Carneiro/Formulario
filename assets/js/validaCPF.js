956f20c3aa3221fc020088b76d9f03cda3eea080
Israel-Carneiro <raelc.oliveira@gmail.com>
2023-03-28 17:05:27 -0300

    Tornando o erro em um erro de validação.

--- assets/js/validaCPF.js
+++ assets/js/validaCPF.js
@@ -1,9 +1,7 @@
 export default function CPFValido(campo) {
     const cpf = campo.value.replace(/\.|-/g, "");
     if (validaNumerosRepetidos(cpf) || validaPrimeiroDigito(cpf) || validaSegundoDigito(cpf)) {
-        console.log("Este cpf não existe!")
-    } else {
-        console.log("Cpf válido.")
+        campo.setCustomValidity('Esse CPF não é válido');
     }
 }
 
